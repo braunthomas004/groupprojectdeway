e59bebd8eec27d86e2506311b5f16b5500282663
Thomas Braun <braunthomas004@gmail.com>
2018-12-18 16:53:28 -0500

    v4.0 small changes here and there.

--- rebuildone/rebuildone/ClientApp/src/app/modules/material.module.ts
+++ rebuildone/rebuildone/ClientApp/src/app/modules/material.module.ts
@@ -3,9 +3,10 @@ import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatSelectModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatSelectModule],
 })
 export class MaterialModule { }
